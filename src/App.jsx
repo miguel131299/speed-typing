b8aa171d60a9c9b7054cdf83d0fc34e7993caa66
Miguel Marcano <miguel131299@gmail.com>
2022-09-25 22:22:01 +0200

    Add File Structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Title</h1>
+      <textarea />
+      <h4>Time Remainning</h4>
+      <button>Start</button>
+      <h1>Word Count</h1>
+    </div>
+  );
+}
+
+export default App;
